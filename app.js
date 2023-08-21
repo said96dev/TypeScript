7990bd82ab11f09d1233352c180645d932556e5c
said96dev <alhendisaid@gmail.com>
2023-08-21 21:00:31 +0200

    generic type

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'SAID',
-    age: 44,
-    greet(phrase) {
-        console.log(phrase + '' + user1.name);
-    },
-};
-user1.greet('Hey there i am ');
+const names = ['Said', 'Edaa'];
+names[1].split('');
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is Done');
+    }, 2000);
+});
+promise.then((data) => {
+    data.split('');
+});
+//Generic functions
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
+}
+const mergeObj = merge({ name: 'Max' }, { age: '30' });
+const mergeObj2 = merge({ name: 'Max', hobbies: ['Sport'] }, { age: '30' });
+console.log(mergeObj.age);
+console.log(mergeObj2);
+function countAndPrint(element) {
+    let descriptionText = 'Got no Value.';
+    if (element.length > 0)
+        descriptionText = 'Got ' + element.length + ' value';
+    return [element, descriptionText];
+}
+console.log(countAndPrint('String'));
+function extractAndConver(obj, key) {
+    return obj[key];
+}
+console.log(extractAndConver({ name: 'MAX' }, 'name'));
+//Generic class
+class DateStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItem() {
+        return [...this.data];
+    }
+}
+const textStorage = new DateStorage();
+textStorage.addItem('SAID');
+textStorage.addItem('MAX');
+textStorage.removeItem('SAID');
+console.log(textStorage.getItem());
--- app.ts
+++ app.ts
@@ -1,17 +1,67 @@
-interface Person {
-  name: string
-  age: number
+const names: Array<string> = ['Said', 'Edaa']
 
-  greet(phrase: string): void
+names[1].split('')
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is Done')
+  }, 2000)
+})
+
+promise.then((data) => {
+  data.split('')
+})
+
+//Generic functions
+function merge<A extends object, B extends object>(obj1: A, obj2: B) {
+  return Object.assign(obj1, obj2)
 }
 
-let user1: Person
+const mergeObj = merge({ name: 'Max' }, { age: '30' })
+const mergeObj2 = merge({ name: 'Max', hobbies: ['Sport'] }, { age: '30' })
+console.log(mergeObj.age)
+console.log(mergeObj2)
 
-user1 = {
-  name: 'SAID',
-  age: 44,
-  greet(phrase: string) {
-    console.log(phrase + '' + user1.name)
-  },
+// Another Generic Function
+
+interface Lengthy {
+  length: number
 }
-user1.greet('Hey there i am ')
+function countAndPrint<T extends Lengthy>(element: T) {
+  let descriptionText = 'Got no Value.'
+  if (element.length > 0) descriptionText = 'Got ' + element.length + ' value'
+  return [element, descriptionText]
+}
+
+console.log(countAndPrint('String'))
+
+function extractAndConver<T extends Object, U extends keyof T>(obj: T, key: U) {
+  return obj[key]
+}
+
+console.log(extractAndConver({ name: 'MAX' }, 'name'))
+
+//Generic class
+
+class DateStorage<T> {
+  private data: T[] = []
+  addItem(item: T) {
+    this.data.push(item)
+  }
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItem() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DateStorage<string>()
+
+textStorage.addItem('SAID')
+textStorage.addItem('MAX')
+
+textStorage.removeItem('SAID')
+
+console.log(textStorage.getItem())
